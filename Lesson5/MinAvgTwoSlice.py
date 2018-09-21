1ea91e497d6535acf3406d954a78095fd356b5d5
david.mumladze <dmuml10@gmail.com>
2018-09-21 15:13:50 +0300

    Finished GenomicRangeQuery solution

--- Lesson5/MinAvgTwoSlice.py
+++ Lesson5/MinAvgTwoSlice.py
@@ -1,5 +1,18 @@
 #Prefix Sums
 
 def solution(A):
+
+    min = 10000
+    index = 0
     for i in range(len(A)):
-        pass
+        if i < len(A) - 1:
+            twoElem = (A[i] + A[i+1])/2
+            if twoElem < min:
+                min = twoElem
+                index = i
+        if i < len(A) - 2:
+            threeElem = (A[i] + A[i+1] + A[i + 2])/3
+            if threeElem < min:
+                min = threeElem
+                index = i
+    return index
