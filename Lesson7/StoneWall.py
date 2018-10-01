60719982bb5c650d202891a785c6670948a00988
david.mumladze <dmuml10@gmail.com>
2018-10-01 15:55:04 +0300

    Finished StoneWall solution

--- Lesson7/StoneWall.py
+++ Lesson7/StoneWall.py
@@ -1,1 +1,16 @@
 #Stacks and Queues
+
+def solution(H):
+    stack = []
+    count = 0
+
+    for i in range(len(H)):
+        while len(stack) != 0 and stack[-1] > H[i]:
+            stack.pop()
+        if len(stack) != 0 and stack[-1] == H[i]:
+            pass
+        else:
+            count += 1
+            stack.append(H[i])
+
+    return count
