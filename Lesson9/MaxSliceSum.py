78b84b5fa539d639b979b2026a18704f7b71896c
david.mumladze <dmuml10@gmail.com>
2018-10-09 15:46:07 +0300

    Finished MaxSliceSum solution

--- Lesson9/MaxSliceSum.py
+++ Lesson9/MaxSliceSum.py
@@ -1,17 +1,14 @@
 #Maximum slice problem
-import sys
 
 def solution(A):
     result = maxElement = -1000000
     temp = 0
-
     for i in A:
-        temp =  max(0, temp + i)
-        result = max(result , temp)
+        temp = max(0, temp + i)
+        result = max(result, temp)
         maxElement = max(maxElement, i)
-    result = max(result, maxElement)
+    if result != 0:
+        result = max(result, maxElement)
+    else:
+        result = maxElement
     return result
-
-print(solution([3,2,-6,4,0]))
-print(solution([-10]))
-print(solution([-2, 1]))
